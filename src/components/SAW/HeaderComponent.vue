a21ed40ea4cf51f55382cee016ef85cea69013c8
fadlymuhammadirvan <fadly.m.irvan@gmail.com>
2025-01-02 23:49:15 +0700

    update header component

--- src/components/SAW/HeaderComponent.vue
+++ src/components/SAW/HeaderComponent.vue
@@ -1,5 +1,11 @@
+<script>
+export default {
+  name: 'HeaderComponent',
+}
+</script>
+
 <template>
-  <header class="text-center mb-4 py-3 rounded bg-light">
-    <h1 class="h3 mb-0"></h1>
+  <header class="text-center mb-4 py-3 bg-light rounded">
+    <h1 class="h3 mb-0">Simple Additive Weight (SAW) Calculator</h1>
   </header>
 </template>
