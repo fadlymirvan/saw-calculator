55a96d497ebce153e5c9f47946b9cf91a7e39328
fadlymuhammadirvan <fadly.m.irvan@gmail.com>
2025-01-02 21:58:23 +0700

    create SAW Calculator component

---
+++ src/components/SAW/HeaderComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="text-center mb-4 py-3 rounded bg-light">
+    <h1 class="h3 mb-0"></h1>
+  </header>
+</template>
---
+++ src/components/SawCalculator.vue
@@ -0,0 +1,23 @@
+<script>
+import HeaderComponent from "@/components/SAW/HeaderComponent.vue";
+
+export default {
+  name: 'SawCalculator',
+  components: {HeaderComponent},
+  props: {},
+  methods: {},
+  data() {}
+}
+</script>
+
+<template>
+  <div class="saw-calculator container py-4">
+    <HeaderComponent />
+  </div>
+</template>
+
+<style scoped>
+.saw-calculator {
+  max-width: 1200px;
+}
+</style>
